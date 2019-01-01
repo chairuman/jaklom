91f792e75c2260dc2585ef99a92fba9e38476abb
rahmad zahri <rahmadzahri007@gmail.com>
2019-01-01 22:49:04 +0700

    halaman login untuk admin (frontend)

---
+++ ureungpo/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Masuk | Jaklom</title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<link rel="stylesheet" href="../css/bootstrap.css">
+		<link rel="stylesheet" href="../css/font-awesome.css">
+		<link rel="stylesheet" href="../css/ionicons.css">
+		<link rel="stylesheet" href="../css/AdminLTE.css">
+		<link rel="stylesheet" href="../css/blue.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+	</head>
+	<body class="hold-transition login-page">
+		<div class="login-box">
+			<div class="login-logo">
+				<a href="../login.php"><b>Jak</b>Lom</a><br><span class="small">Admin Login</span>
+			</div>
+			<div class="login-box-body">
+			    <p class="login-box-msg">silakan masuk dengan akun anda</p>
+			    <form action="" method="POST">
+				    <div class="form-group has-feedback">
+				        <input type="text" name="username" class="form-control" placeholder="username atau email" required>
+				        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+				    </div>
+				    <div class="form-group has-feedback">
+				        <input type="password" name="password" class="form-control" placeholder="Password" required>
+				        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				    </div>
+				    <div class="row">
+				        <div class="col-xs-8">
+				        </div>
+				        <div class="col-xs-4">
+				          <button type="submit" name="login" class="btn btn-primary btn-block btn-flat">Masuk</button>
+				        </div>
+				    </div>
+			    </form>
+			</div>
+		</div>
+
+		<script src="../js/jquery.js"></script>
+		<script src="../js/bootstrap.js"></script>
+		<script src="../js/icheck.js"></script>
+	</body>
+</html>
